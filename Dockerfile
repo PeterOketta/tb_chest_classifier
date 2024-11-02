a20ee4c3813f83cf4d4239321648d0fe9f6cfe6f
PeterOketta <97467356+PeterOketta@users.noreply.github.com>
2024-11-02 20:52:35 +0300

    model loading

--- Dockerfile
+++ Dockerfile
@@ -5,21 +5,12 @@ FROM python:3.9-slim
 ENV PYTHONUNBUFFERED=1
 ENV PYTHONDONTWRITEBYTECODE=1
 
-# Install curl and other required system dependencies
-RUN apt-get update && \
-    apt-get install -y curl && \
-    rm -rf /var/lib/apt/lists/*
-
 # Set the working directory
 WORKDIR /app
 
 # Copy requirements.txt into the container and install dependencies
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-
-# Download the model file using the pre-authenticated URL
-RUN mkdir -p model && \
-    curl -L -o model/classification_model.keras "https://storage.cloud.google.com/classification_model_tb/classification_model.keras"
 
 # Copy the entire project directory into the container
 COPY . .
--- app/model_loader.py
+++ app/model_loader.py
@@ -1,17 +1,33 @@
-import tensorflow as tf
+import os
+import requests
 from tensorflow.keras.models import load_model as keras_load_model
 from PIL import Image
 import numpy as np
-import os
 
-
+# Path to the model file within the Docker container
 MODEL_PATH = "model/classification_model.keras"
+CLASS_NAMES = ["Active TB", "Inactive or Healed TB", "Normal", "Others"]
 
-CLASS_NAMES = [" Active TB", "Inactive or Healed TB", "Normal","Others"]
+# Pre-authenticated URL to download the model
+MODEL_URL = "https://storage.cloud.google.com/classification_model_tb/classification_model.keras"
+
+def download_model():
+    """Download the model from a pre-authenticated URL if it's not present."""
+    if not os.path.exists(MODEL_PATH):
+        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+        print("Downloading model...")
+        response = requests.get(MODEL_URL)
+        if response.status_code == 200:
+            with open(MODEL_PATH, 'wb') as f:
+                f.write(response.content)
+            print("Model downloaded successfully.")
+        else:
+            raise RuntimeError(f"Failed to download model: {response.status_code}")
 
 def load_model():
-    if not os.path.exists(MODEL_PATH):
-        raise FileNotFoundError(f"Model file not found at path: {MODEL_PATH}")
+    # Ensure the model file is downloaded
+    download_model()
+    
     try:
         model = keras_load_model(MODEL_PATH)
         return model
