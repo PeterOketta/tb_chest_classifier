67c3ac2d0c3d64c1fd10566a0e7b8ab503a63e1b
PeterOketta <97467356+PeterOketta@users.noreply.github.com>
2024-11-02 11:17:18 +0300

    added model inferencing

---
+++ app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+from app.model_loader import load_model, preprocess_image, CLASS_NAMES
+
+app = FastAPI()
+
+try:
+    model = load_model()
+except FileNotFoundError as e:
+    raise RuntimeError(f"Model could not be loaded: {e}")
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    try:
+        if file.content_type not in ["image/jpeg", "image/png","image/jpg"]:
+            raise HTTPException(status_code=400, detail="Invalid file format. Use JPEG or PNG.")
+        
+        # Open and preprocess the image
+        image = Image.open(file.file)
+        processed_image = preprocess_image(image)
+        prediction = model.predict(processed_image)
+        
+        predicted_index = np.argmax(prediction, axis=1)[0]
+        predicted_class = CLASS_NAMES[predicted_index]
+        
+        return {"prediction": predicted_class}
+    
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ app/model_loader.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model as keras_load_model
+from PIL import Image
+import numpy as np
+import os
+
+
+MODEL_PATH = "model/classification_model.keras"
+
+CLASS_NAMES = [" Active TB", "Inactive or Healed TB", "Normal","Others"]
+
+def load_model():
+    if not os.path.exists(MODEL_PATH):
+        raise FileNotFoundError(f"Model file not found at path: {MODEL_PATH}")
+    try:
+        model = keras_load_model(MODEL_PATH)
+        return model
+    except Exception as e:
+        raise RuntimeError(f"Failed to load model: {e}")
+
+def preprocess_image(image: Image.Image):
+    img_width, img_height = 150, 150
+    image = image.resize((img_width, img_height))
+    image_array = np.array(image) / 255.0
+    image_array = np.expand_dims(image_array, axis=0)
+    return image_array
